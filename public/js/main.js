d2c2b32e6ec4b085a88348cbe18836e5ecae1727
Mariojoestar33 <mariano.pena15@gmail.com>
2024-06-06 10:33:05 -0600

    Implementacion de Layouts y dinamica para el mapa.

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,40 @@
-var map = L.map('map').setView([19.51132499810658, -99.12652589999172], 13)
+// Creacion del mapa y asignacion de la ubicacion principal
+var map = L.map('map').setView([19.51132499810658, -99.12652589999172], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map)
+}).addTo(map);
 
+// Asignacion de marcadores
 var marker = L.marker([19.51132499810658, -99.12652589999172]).addTo(map);
 
+// Marcador en el nodo de la UPIITA
 marker.bindPopup("Nodo Principal UPIITA").openPopup();
-//circle.bindPopup("I am a circle.");
-//polygon.bindPopup("I am a polygon.");
 
-var popup = L.popup();
-
+// Funcion para desplegar coordenadas al hacer click en el mapa
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("La latitud y longitud es de: " + e.latlng.toString())
+        .setContent("Coordenadas: " + e.latlng.toString())
         .openOn(map);
 }
-
 map.on('click', onMapClick);
+
+// Funcion para actualizar el sidebar con los detalles del marcador
+function updateSidebar(markerInfo) {
+    var sidebar = document.getElementById('marker-details');
+    sidebar.innerHTML = `
+        <p><strong>Nombre:</strong> ${markerInfo.name}</p>
+        <p><strong>Coordenadas:</strong> ${markerInfo.coords.lat}, ${markerInfo.coords.lng}</p>
+    `;
+}
+
+// Evento de clic en el marcador para mostrar detalles en el sidebar
+marker.on('click', function(e) {
+    var markerInfo = {
+        name: "Nodo Principal UPIITA",
+        coords: e.latlng
+    };
+    updateSidebar(markerInfo);
+});
